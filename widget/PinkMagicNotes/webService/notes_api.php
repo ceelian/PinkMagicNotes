c342d858d59d726b5d4fffac8812c231fe30420e
Karin Pichler <karin@Maximilian.(none)>
2010-12-14 20:05:24 +0100

    added first webserver functionality + dummy json file

--- widget/PinkMagicNotes/webService/notes_api.php
+++ widget/PinkMagicNotes/webService/notes_api.php
@@ -9,6 +9,7 @@
  * - Errors can be user caused (such as invalid user inputs)
  * - Errors can be server caused (internal server errors, connection timeouts, DB exceptions etc..)
  */
+
 	
 	$action = isset($_GET['action']) ? $_GET['action'] : '';
 	$response = "";
@@ -19,6 +20,13 @@
 		switch($action) {
 			case 'getAllNotes'
 				$response = NotesService::getAllNotes();
+			break;
+
+			case 'getSpecificNote'
+				if(!isset($_GET['notes_id']) || $_GET)
+					throw new Exception("Notes-ID is undefined.");
+				else
+					$response = NotesService::getSpecificNote($_GET['notes_id']);
 			break;
 			
 			default:
@@ -50,11 +58,12 @@
  * returns all responses as JSON
  */
 class NotesService {
+    private static $filename = "notes.json";
 	private static function readfile($filename) {
                 $file = fopen($filename,"r");
-                $contents = fread($file, filesize($filename));
+                $content = fread($file, filesize($filename));
                 fclose($file);
-		return $contents;
+		        return $content;
 	}
 
 	private static function arrayToJson($arr) {
@@ -62,9 +71,20 @@ class NotesService {
 	}
 	
 	public static function getAllNotes() {
-		return readfile("notes.json");
-
+		return readfile(self::$filename);
 	}
+
+    public static function getSpecificNote($note_id) {
+        echo self::$filename;
+        $content = readfile(self::$filename);
+        echo "after readfile";
+        $content = trim($content);
+        echo $content;
+        echo "after trim";
+        #echo $content;
+        #$php_content = json_decode($content);
+        #echo $php_content;
+    }
 }
 	
 ?>
