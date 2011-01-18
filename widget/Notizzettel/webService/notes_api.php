f1f1d96131dfb6b34dde72a8150739a4e2b4c776
Christian Haintz <ceelian@gmx.net>
2011-01-18 17:18:56 +0100

    added pretty print to webservice

--- widget/Notizzettel/webService/notes_api.php
+++ widget/Notizzettel/webService/notes_api.php
@@ -105,7 +105,7 @@ class NotesService {
     }
 
 	private static function arrayToJson($arr) {
-		return json_encode($arr);
+		return self::json_format(json_encode($arr));
 	}
 	
 	public static function getAllNotes() {
@@ -141,7 +141,7 @@ class NotesService {
             }
         }
         $php_content['notes'] = $notes;
-        $content = json_encode($php_content);
+        $content = self::arrayToJson($php_content);
         self::writeFileContent(self::$filename, $content);
     }
 
@@ -151,11 +151,12 @@ class NotesService {
 		$php_content = json_decode($content,TRUE);
 		$notes = $php_content['notes'];
 		if ($uuid == '') $uuid = self::uuid();
-		error_log($json_note);
+		//error_log($json_note);
 		$new_note = json_decode($json_note,TRUE);
 		$notes[$uuid] = $new_note;
-		print $new_note;
-		return self::arrayToJson($notes);
+		//#rint $new_note;
+		$content = self::arrayToJson($notes);
+		self::writeFileContent(self::$filename, $content);
 	
 	}
 
@@ -203,6 +204,84 @@ class NotesService {
 		    mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535) // 48 bits for "node" 
 		); 
 	}
+
+	private static function json_format($json)
+	{
+		$tab = "  ";
+		$new_json = "";
+		$indent_level = 0;
+		$in_string = false;
+
+		$json_obj = json_decode($json);
+
+		if($json_obj === false)
+		    return false;
+
+		$json = json_encode($json_obj);
+		$len = strlen($json);
+
+		for($c = 0; $c < $len; $c++)
+		{
+		    $char = $json[$c];
+		    switch($char)
+		    {
+		        case '{':
+		        case '[':
+		            if(!$in_string)
+		            {
+		                $new_json .= $char . "\n" . str_repeat($tab, $indent_level+1);
+		                $indent_level++;
+		            }
+		            else
+		            {
+		                $new_json .= $char;
+		            }
+		            break;
+		        case '}':
+		        case ']':
+		            if(!$in_string)
+		            {
+		                $indent_level--;
+		                $new_json .= "\n" . str_repeat($tab, $indent_level) . $char;
+		            }
+		            else
+		            {
+		                $new_json .= $char;
+		            }
+		            break;
+		        case ',':
+		            if(!$in_string)
+		            {
+		                $new_json .= ",\n" . str_repeat($tab, $indent_level);
+		            }
+		            else
+		            {
+		                $new_json .= $char;
+		            }
+		            break;
+		        case ':':
+		            if(!$in_string)
+		            {
+		                $new_json .= ": ";
+		            }
+		            else
+		            {
+		                $new_json .= $char;
+		            }
+		            break;
+		        case '"':
+		            if($c > 0 && $json[$c-1] != '\\')
+		            {
+		                $in_string = !$in_string;
+		            }
+		        default:
+		            $new_json .= $char;
+		            break;                   
+		    }
+		}
+
+		return $new_json;
+	} 
 }
 	
 ?>
