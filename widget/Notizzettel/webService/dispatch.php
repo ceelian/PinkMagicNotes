b53eb798f9899bd9e8b5dd2c4ef877f2bd1cad23
Christian Haintz <ceelian@gmx.net>
2011-01-20 16:46:58 +0100

    improved TUG support and enhanced tonic to parse url fields automatically

--- widget/Notizzettel/webService/dispatch.php
+++ widget/Notizzettel/webService/dispatch.php
@@ -1,6 +1,13 @@
 <?php
+//print_r($_SERVER);
+//die();
 
-require_once './restwrapper.php';
+#patch to support also not apache2 rewrite webservers (eg. TU-Graz)
+#just give the restresource with the dispatch.php as endpoint
+#http://www.restserver.org/dispatch.php/helloworld/asd/?var1=haha&var2=aa
+if (!isset($_SERVER['REDIRECT_URL'])){
+	$_SERVER['REDIRECT_URL']=$_SERVER['PATH_INFO'];
+}
 
 // load Tonic library
 require_once './lib/tonic.php';
@@ -9,6 +16,8 @@ require_once './lib/tonic.php';
 require_once './helloworld/helloworld.php';
 
 // handle request
+$config = array();
+$config['baseUri']="/dispatch.php";
 $request = new Request();
 $resource = $request->loadResource();
 $response = $resource->exec($request);
